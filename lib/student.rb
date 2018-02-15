9fd8cc2c762a7f6c605ea4ee44a843a487e08414
Sharmina Akhtar <sakht318@gmail.com>
2018-02-15 17:54:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT students.name, students.id
+      SELECT , students.id, students.name
       FROM students
       WHERE grade = 10;
     SQL
